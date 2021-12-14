7d2e4e0d28117607958d603fd51bf0db8557bcc1
Yangyue Wang <yangyuewang123@gmail.com>
2021-12-13 19:33:38 -0500

    Initial Commit

---
+++ Shared/ControleMiniApp.swift
@@ -0,0 +1,17 @@
+//
+//  ControleMiniApp.swift
+//  Shared
+//
+//  Created by Yangyue Wang on 12/13/21.
+//
+
+import SwiftUI
+
+@main
+struct ControleMiniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
